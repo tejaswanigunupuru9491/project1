db26ef3c0efc60dff842cb7e1302f33632fd0222
teja9491 <gunupurutejaswani@gmail.com>
2025-06-19 22:26:56 +0530

    intial commit of project1

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    _id:{type: String,required:true},
+    name:{type: String,required:true},
+    email:{type: String,required:true},
+    image:{type: String,required:true}
+    
+})
+const user=mongoose.model('user',userSchema)
+export default User;
