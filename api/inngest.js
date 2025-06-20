3b2cb7bd03c54f5d008e394a35d8b6546d1d0e10
teja9491 <gunupurutejaswani@gmail.com>
2025-06-20 13:33:57 +0530

    p2

--- api/inngest.js
+++
@@ -1,7 +0,0 @@
-// api/inngest.js
-
-import { serve } from "inngest/vercel"; // For Vercel deployment
-import { inngest, functions } from "../server/inngest/index.js";
-
-
-export default serve(inngest, functions);
