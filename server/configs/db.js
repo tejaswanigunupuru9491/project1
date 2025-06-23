21681c31a3a4dd8014f116429d0f5c28cabe9656
teja9491 <gunupurutejaswani@gmail.com>
2025-06-23 20:57:10 +0530

    tp1

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,8 @@ const connectDB =async() =>
 {
     try{
         mongoose.connection.on('connected',()=> console.log('Databaseconnected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/Project 0`)
+       await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`);
+
     }
     catch(error){
         console.log(error.message);
